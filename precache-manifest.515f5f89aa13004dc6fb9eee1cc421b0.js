dcc8c74d46b845359e02b35d59d9458873fb8bb9
mdmunna <rgcet6022@gmail.com>
2020-03-15 15:24:14 +0900

    Updates

---
+++ precache-manifest.515f5f89aa13004dc6fb9eee1cc421b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2992c003803b0ef8516edd5b47634111",
+    "url": "/md-ToDo-App-React/index.html"
+  },
+  {
+    "revision": "2775c47acfdf55f378cc",
+    "url": "/md-ToDo-App-React/static/css/main.fd7ab34e.chunk.css"
+  },
+  {
+    "revision": "144a734247babaca625d",
+    "url": "/md-ToDo-App-React/static/js/2.ae153c62.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/md-ToDo-App-React/static/js/2.ae153c62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2775c47acfdf55f378cc",
+    "url": "/md-ToDo-App-React/static/js/main.3039ecd9.chunk.js"
+  },
+  {
+    "revision": "da610970ddd170c0a94e",
+    "url": "/md-ToDo-App-React/static/js/runtime-main.af94d6b2.js"
+  }
+]);
